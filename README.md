b34e60e114990d2b55fc748b5a6bef33bfe3a7f1
SaraiFerrer <87380666+SaraiFerrer@users.noreply.github.com>
2022-09-26 12:31:10 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PRO68-
+# wily-v2-PRO-C68
+Código de solución para PRO-C68
---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+
+const Tab = createBottomTabNavigator();
+
+export default class BottomTabNavigator extends Component {
+  
+}
